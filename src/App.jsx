e4c515ee10e7da1a9068da9498920724a50e5130
saljaoui <soufian2022ff@gmail.com>
2025-07-14 19:35:42 +0100

    start_project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#212121] text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">soufian</h1>
+    <div className="min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-xl font-bold">soufian</h1>
     </div>
   );
 }
