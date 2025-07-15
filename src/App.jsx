e086baef5944c455b5f3c011032dc93b39f3413e
mbakhouc <bakhouch2019@gmail.com>
2025-07-15 15:19:50 +0100

    adding necessery files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import SidebarControls from './components/SidebarControls.jsx'
-import Topbar from './components/Topbar.jsx'
+import React from 'react';
+import Topbar from './components/Topbar';
+import SidebarControls from './components/SidebarControls';
+import LayoutPreview from './components/LayoutPreview';
+import GeneratedCode from './components/GeneratedCode';
+import './styles/main.css';
 
 function App() {
   return (
-  <div className='app'>
-    <Topbar />
-    <div className="content-main">
-      <SidebarControls />
-      
+    <div className="app-container">
+      <Topbar />
+      <div className="main-content">
+        <SidebarControls />
+        <LayoutPreview />
+        <GeneratedCode />
+      </div>
     </div>
-  </div>
-    
   );
 }
 
